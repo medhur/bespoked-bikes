c6236bef4aa015ad32e07ced4d5f682695900fb8
meena.dhurshetty <meena.dhurshetty@speridian.com>
2024-12-01 22:54:19 +0530

    Initial push of client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RoutesComponent from './routes/RoutesComponent';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <RoutesComponent /> {/* This will render the page based on URL */}
+        <ToastContainer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
